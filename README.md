189a46f759c8ba344f781c979617b26da4636a28
martin <martin@martin.desktop>
2020-03-03 16:26:55 +0100

    added install script and general_requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # install
 Repo that house install scripts for Ubuntu 18.04
+
+run 
+```bash
+sh install.sh
+```
+and follow guidelines
---
+++ install.sh
@@ -0,0 +1,40 @@
+mkdir tmp
+cd tmp
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Pycharm
+sudo snap install pycharm-community --classic
+
+# Mailspring
+sudo snap install mailspring
+
+# VLC
+sudo snap install VLC
+
+# Netron
+sudo snap install netron
+
+# Spotify
+ubuntu-software --search spotify
+
+# Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+sha256sum Anaconda*
+bash Anaconda3*
+
+# OpenCV
+sudo apt update
+sudo apt install -y python3-opencv
+
+# Create anaconda environment with cuda for tensorflow-gpu and all necessary packages
+conda create --name ml tensorflow-gpu
+echo "Actiate conda environment and run\npip instal -r general_requirements.txt\n"
+
+
+
