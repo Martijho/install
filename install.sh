d3acf92352aeeb7103a284ab231604547c18d6ee
martin <martin@martin.desktop>
2020-03-04 13:44:17 +0100

    added ffmpeg

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ sudo apt-get upgrade
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
 
+# FFMPEG
+sudo apt install -y ffmpeg
 # Pycharm
 sudo snap install pycharm-community --classic
 
